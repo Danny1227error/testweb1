e1b2f791ff9c289a2ef8bef4c539572c42ec1d17
Danny1227error <yuwadeeple7@gmail.com>
2024-12-11 13:17:52 +0700

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['your-image-domain.com'],
+        unoptimized: true
+      }
+};
+
+export default nextConfig;
